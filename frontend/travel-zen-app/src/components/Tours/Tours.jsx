233f418c5a3a4babdf4bf78e388275fca7ab4184
kathy Del Castillo <kathydcastillo90@gmail.com>
2024-05-30 01:15:34 -0400

    updated tour page and css

--- frontend/travel-zen-app/src/components/Tours/Tours.jsx
+++ frontend/travel-zen-app/src/components/Tours/Tours.jsx
@@ -11,6 +11,20 @@ export default function Tours({tours, setTours, }) {
      <img className='mainPic' src='https://silversea-discover.imgix.net/2023/08/muchsmallerhero.jpg?auto=compress%2Cformat&ixlib=php-3.3.1' />
      <div className='allTours'>All Tours</div>
     </div>
+    <div className="searchBar">
+     <img className='travPic' src='https://img.freepik.com/free-photo/tourist-doing-surprise-gesture_1368-7048.jpg?size=626&ext=jpg&ga=GA1.1.1539979737.1708785603&semt=ais_user' />
+        <form className='searchForm'>
+            <label className="searchLabel">Destination</label>
+            <input className="searchInput" type="text" name="destination" placeholder="Search"/>
+            <label className="searchLabel">Departure</label>
+            <input className="searchInput" type="date" name="date"/>
+            <label className="searchLabel">Return</label>
+            <input className="searchInput" type="date" name="date"/>
+            <button className="searchBtn" type="submit">Search</button>
+        </form>
+    </div>
+    <h1 className='tourHeader'>All Inclusive Resorts</h1>
+
     <div className="toursInner">
         {tours.map(tour => (
           <div className="eachTour" key={tour._id}>
@@ -23,12 +37,6 @@ export default function Tours({tours, setTours, }) {
           </div>
         ))}
       </div>
-    
-     <img className='travPic' src='https://img.freepik.com/free-photo/tourist-doing-surprise-gesture_1368-7048.jpg?size=626&ext=jpg&ga=GA1.1.1539979737.1708785603&semt=ais_user' />
-        
-
-
-   
 
     </div>
   );
